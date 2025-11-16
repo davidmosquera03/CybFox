2ebb09a7eafa8834e9dc77df727c7f4e66de8309
Ahiruk <sanotyal@uninorte.edu.co>
2025-11-16 15:01:34 -0500

    vite.config act

--- extension.security/frontend/vite.config.js
+++ extension.security/frontend/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {
+  defineConfig
+} from 'vite'
+import {
+  svelte
+} from '@sveltejs/vite-plugin-svelte'
+import {
+  resolve
+} from 'path'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: '../dist', // compilamos directo a extension.security/dist
+    emptyOutDir: false, // no borra todo lo que ya tengas
+    rollupOptions: {
+      input: {
+        blocked: resolve(__dirname, 'src/blocked.html'),
+        popup: resolve(__dirname, 'src/popup.html')
+      }
+    }
+  }
 })
